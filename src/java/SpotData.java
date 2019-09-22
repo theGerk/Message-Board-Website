3cb76f416c1f201075ccfd5fda7d13b4e79b99db
Jack Dawkins <jdawkins02@hamline.edu>
2019-09-22 11:40:04 -0500

    Defined sql databases in init.sql. Began CommentData.java and SpotData.java

---
+++ src/java/SpotData.java
@@ -0,0 +1,58 @@
+
+/*
+ * A class storing the data needed
+ * for the webpage for a spot.
+ * 
+ * When a user navigates to a spot's
+ * webpage, an instance of this
+ * class is initialized for the spot.
+ * Then this class' getters are
+ * used to build the webpage.
+ */
+
+import java.util.List;
+
+import Comment;
+
+public class SpotData{
+
+	/*
+	 * The id of this spot
+	 * in the 'Spots' sql table.
+	 */
+
+	private long int uniqueId;
+
+	/*
+	 * The latitude and
+	 * longitude of the spot.
+	 */
+
+	private float longitude, latitude;
+
+	/*
+	 * The most relevant n comments on this
+	 * spot.
+	 */
+
+	private List< Comment > comments;
+
+	/*
+	 * The main constructor initializes
+	 * the above variables from the given
+	 * uniqueId. It loads
+	 * numComments comments.
+	 */
+
+	public SpotData( int uniqueIdTmp, int numComments ){
+
+		uniqueId = uniqueIdTmp;
+
+
+
+
+
+
+
+
+
